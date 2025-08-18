41c1d45516ff6df343c0d1da9db1e67274dbd8df
Srinivas <kurumallasrinivas123@gmail.com>
2025-08-18 22:37:05 +0530

    D50 - missed to add DFS to right side view

--- JS/Medium/BinaryTree/LevelOrderTraversal/rightSideView.js
+++ JS/Medium/BinaryTree/LevelOrderTraversal/rightSideView.js
@@ -20,13 +20,29 @@ var rightSideView = function (root) {
     let levelSize = q.length;
     for (let i = 0; i < levelSize; i++) {
       let curr = q.shift();
+      if (i == levelSize - 1) result.push(curr.val);
 
       curr.left && q.push(curr.left);
       curr.right && q.push(curr.right);
-      result.push(curr.val);
     }
   }
   return result;
+};
+
+var rightSideView = function (root) {
+  let res = [];
+
+  let traverse = (curr, depth) => {
+    if (!curr) return;
+    if (res.length == depth) {
+      res.push(curr.val);
+    }
+
+    traverse(curr.right, depth + 1);
+    traverse(curr.left, depth + 1);
+  };
+  traverse(root, 0);
+  return res;
 };
 let vals = [1, 2, 3, null, 5, null, 4];
 console.log(rightSideView(vals));
