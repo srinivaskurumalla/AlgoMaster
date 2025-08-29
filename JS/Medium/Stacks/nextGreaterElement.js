1ef18b99373c9d7badd16ceafa72546b7a7701d5
Srinivas <kurumallasrinivas123@gmail.com>
2025-08-29 23:01:27 +0530

    D61

--- JS/Medium/Stacks/nextGreaterElement.js
+++ JS/Medium/Stacks/nextGreaterElement.js
@@ -4,31 +4,25 @@
  * @return {number[]}
  */
 var nextGreaterElement = function (nums1, nums2) {
-  let map = {};
-  let res = [];
-  let stack = [];
+  const map = {};
+  const stack = []; // Monotonic decreasing stack
   for (let i = nums2.length - 1; i >= 0; i--) {
-    if (stack.length == 0) {
-      map[nums2[i]] = -1;
-    } else {
-      let top = stack[stack.length - 1];
-      while (stack.length && top < nums2[i]) {
-        stack.pop();
-        top = stack[stack.length - 1];
-      }
-      if (top) {
-        map[nums2[i]] = top;
-      } else {
-        map[nums2[i]] = -1;
-      }
+    const num = nums2[i];
+
+    // Pop all elements smaller or equal to current number
+    while (stack.length && stack[stack.length - 1] <= num) {
+      stack.pop();
     }
+    // If stack is empty, no greater element exists
+    map[num] = stack.length ? stack[stack.length - 1] : -1;
+
+    // Push current number onto stack
     stack.push(nums2[i]);
   }
-//  console.log(map);
+  //  console.log(map);
 
-  for (let i = 0; i < nums1.length; i++) {
-    res.push(map[nums1[i]]);
-  }
+  // Build result for nums1 based on the map
+  return nums1.map((num) => map[num]);
   //console.log(res);
   return res;
 };
