e522f28e8bf95981350a6939d0fe49604077999d
srinivaskurumalla <kurumallasrinivas123@gmail.com>
2025-10-05 07:58:27 +0530

    Modified the paths

--- C#/DSA/obj/Debug/net9.0/DSA.AssemblyInfo.cs
+++ C#/DSA/obj/Debug/net9.0/DSA.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DSA")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+24600fe5381f6524a8d70a880d0264a237f24cc5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a0e4be0b545df2e1bfa05a6e00e6407522819eed")]
 [assembly: System.Reflection.AssemblyProductAttribute("DSA")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DSA")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
